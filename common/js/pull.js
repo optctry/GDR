1a471594f5cfca5c82b78beb6848efe828a7904c
optctry <optcitaoptcita@gmail.com>
2017-10-14 18:31:47 +0200

    Inserire solo numeri per immagine personaggio

--- common/js/pull.js
+++ common/js/pull.js
@@ -2,73 +2,73 @@ var myimages =[
 // Mob fino a Whiskey Peak
 
 //Str
-{ image: "f0120.png", probability: 0.00000},
-{ image: "f0121.png", probability: 0.00000},
-{ image: "f0126.png", probability: 0.00000},
-{ image: "f0131.png", probability: 0.00000},
-{ image: "f0136.png", probability: 0.00000},
-{ image: "f0165.png", probability: 0.00000},
-{ image: "f0170.png", probability: 0.00000},
-{ image: "f0175.png", probability: 0.00000},
-{ image: "f0179.png", probability: 0.00000},
-{ image: "f0237.png", probability: 0.00000},
+{ image: "0120.png", probability: 0.00000},
+{ image: "0121.png", probability: 0.00000},
+{ image: "0126.png", probability: 0.00000},
+{ image: "0131.png", probability: 0.00000},
+{ image: "0136.png", probability: 0.00000},
+{ image: "0165.png", probability: 0.00000},
+{ image: "0170.png", probability: 0.00000},
+{ image: "0175.png", probability: 0.00000},
+{ image: "0179.png", probability: 0.00000},
+{ image: "0237.png", probability: 0.00000},
   
   
 //qck 
-{ image: "f0127.png", probability: 0.00000},
-{ image: "f0122.png", probability: 0.00000},
-{ image: "f0132.png", probability: 0.00000},
-{ image: "f0137.png", probability: 0.00000},
-{ image: "f0164.png", probability: 0.00000},
-{ image: "f0167.png", probability: 0.00000},
-{ image: "f0172.png", probability: 0.00000},
-{ image: "f0177.png", probability: 0.00000},
-{ image: "f0181.png", probability: 0.00000},
-{ image: "f0239.png", probability: 0.00000},
+{ image: "0127.png", probability: 0.00000},
+{ image: "0122.png", probability: 0.00000},
+{ image: "0132.png", probability: 0.00000},
+{ image: "0137.png", probability: 0.00000},
+{ image: "0164.png", probability: 0.00000},
+{ image: "0167.png", probability: 0.00000},
+{ image: "0172.png", probability: 0.00000},
+{ image: "0177.png", probability: 0.00000},
+{ image: "0181.png", probability: 0.00000},
+{ image: "0239.png", probability: 0.00000},
   
   
 //dex
-{ image: "f0119.png", probability: 0.00000},
-{ image: "f0123.png", probability: 0.00000},
-{ image: "f0128.png", probability: 0.00000},
-{ image: "f0133.png", probability: 0.00000},
-{ image: "f0138.png", probability: 0.00000},
-{ image: "f0163.png", probability: 0.00000},
-{ image: "f0166.png", probability: 0.00000},
-{ image: "f0171.png", probability: 0.00000},
-{ image: "f0176.png", probability: 0.00000},
-{ image: "f0180.png", probability: 0.00000},
-{ image: "f0215.png", probability: 0.00000},
-{ image: "f0238.png", probability: 0.00000},
+{ image: "0119.png", probability: 0.00000},
+{ image: "0123.png", probability: 0.00000},
+{ image: "0128.png", probability: 0.00000},
+{ image: "0133.png", probability: 0.00000},
+{ image: "0138.png", probability: 0.00000},
+{ image: "0163.png", probability: 0.00000},
+{ image: "0166.png", probability: 0.00000},
+{ image: "0171.png", probability: 0.00000},
+{ image: "0176.png", probability: 0.00000},
+{ image: "0180.png", probability: 0.00000},
+{ image: "0215.png", probability: 0.00000},
+{ image: "0238.png", probability: 0.00000},
   
   
 //psy
-{ image: "f0124.png", probability: 0.00000},
-{ image: "f0129.png", probability: 0.00000},
-{ image: "f0134.png", probability: 0.00000},
-{ image: "f0139.png", probability: 0.00000},
-{ image: "f0168.png", probability: 0.00000},
-{ image: "f0173.png", probability: 0.00000},
-{ image: "f0178.png", probability: 0.00000},
-{ image: "f0182.png", probability: 0.00000},
-{ image: "f0240.png", probability: 0.00000},
+{ image: "0124.png", probability: 0.00000},
+{ image: "0129.png", probability: 0.00000},
+{ image: "0134.png", probability: 0.00000},
+{ image: "0139.png", probability: 0.00000},
+{ image: "0168.png", probability: 0.00000},
+{ image: "0173.png", probability: 0.00000},
+{ image: "0178.png", probability: 0.00000},
+{ image: "0182.png", probability: 0.00000},
+{ image: "0240.png", probability: 0.00000},
   
   
 //int
-{ image: "f0125.png", probability: 0.00000},
-{ image: "f0130.png", probability: 0.00000},
-{ image: "f0135.png", probability: 0.00000},
-{ image: "f0140.png", probability: 0.00000},
-{ image: "f0141.png", probability: 0.00000},
-{ image: "f0169.png", probability: 0.00000},
-{ image: "f0174.png", probability: 0.00000},
-{ image: "f0183.png", probability: 0.00000},
-{ image: "f0241.png", probability: 0.00000},
+{ image: "0125.png", probability: 0.00000},
+{ image: "0130.png", probability: 0.00000},
+{ image: "0135.png", probability: 0.00000},
+{ image: "0140.png", probability: 0.00000},
+{ image: "0141.png", probability: 0.00000},
+{ image: "0169.png", probability: 0.00000},
+{ image: "0174.png", probability: 0.00000},
+{ image: "0183.png", probability: 0.00000},
+{ image: "0241.png", probability: 0.00000},
 
 
 // Personaggi unici rimasti
-{ image: "f0407.png", probability: 0.90000},
-{ image: "f0425.png", probability: 0.10000},
+{ image: "0407.png", probability: 0.90000},
+{ image: "0425.png", probability: 0.10000},
 ];
 
 function getImage() {
@@ -84,5 +84,5 @@ function getImage() {
 }
 
 for (var i = 0; i < 1; i++) {
-  document.write('<img src="https://onepiece-treasurecruise.com/wp-content/uploads/' + getImage() +'" border=0>');
+  document.write('<img src="https://onepiece-treasurecruise.com/wp-content/uploads/f' + getImage() +'" border=0>');
 }
